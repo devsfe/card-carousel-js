2089f8b400570f66f30fb04ebf638be2d9202ceb
Fernando Chaves <fernando.chaves16@hotmail.com>
2020-11-11 20:56:58 -0300

    Funçao clicar no ponto mostrar imagem finalizada

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ var btnPrev = document.getElementById('carousel__btn--prev');
 var btnNext = document.getElementById('carousel__btn--next');
 const slides = document.getElementsByClassName('card');
 const totalSlides = slides.length;
-const dot = document.getElementsByClassName('dot');
+const dots = document.getElementsByClassName('dot');
+var dot1 = document.getElementById('dot-1');
+var dot2 = document.getElementById('dot-2');
+var dot3 = document.getElementById('dot-3');
 
 //Eventos
 btnNext.addEventListener('click', moveNextSlide);
 btnPrev.addEventListener('click', movePrevSlide);
+dot1.addEventListener('click', mostrarPrimeiraImagem);
+dot2.addEventListener('click', mostrarSegundaImagem);
+dot3.addEventListener('click', mostrarTerceiraImagem);
 
 
 //Atualizar posição do Slide
@@ -21,13 +27,13 @@ function updateSlidePosition() {
 }
 
 
-//Atualizar função dos pontos
+//Atualizar posição dos pontos
 function updateDot() {
-    for (var ponto of dot) {
-        ponto.classList.remove('dot-active');
+    for (var dot of dots) {
+        dot.classList.remove('dot-active');
     }
 
-    dot[slidePosition].classList.add('dot-active');
+    dots[slidePosition].classList.add('dot-active');
 }
 
 
@@ -50,6 +56,36 @@ function movePrevSlide() {
         slidePosition = totalSlides -1  ; 
     } else {
         slidePosition --;
+    }
+
+    updateDot();
+    updateSlidePosition();
+}
+
+
+// Mostrar imagem ao clicar nos pontos
+
+function mostrarPrimeiraImagem() {
+    if (slidePosition !== 0) {
+        slidePosition = 0; 
+    }
+
+    updateDot();
+    updateSlidePosition();
+}
+
+function mostrarSegundaImagem() {
+    if (slidePosition !== 1) {
+        slidePosition = 1; 
+    }
+
+    updateDot();
+    updateSlidePosition();
+}
+
+function mostrarTerceiraImagem() {
+    if (slidePosition !== 2) {
+        slidePosition = 2; 
     }
 
     updateDot();
